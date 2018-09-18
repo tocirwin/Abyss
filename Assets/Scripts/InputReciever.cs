94b57a287be7c074eb804eefeb38e1b33010242c
Danny Irwin <danny@libertinegames.com>
2018-09-17 23:46:54 -0700

    In process changes to input manager filtering.

--- Assets/Scripts/InputReciever.cs
+++ Assets/Scripts/InputReciever.cs
@@ -8,10 +8,13 @@ public class InputReciever : MonoBehaviour {
 	public event EventHandler<InputEventArgs> InputDetected;
 	public event EventHandler<InputEventArgs> InputReleased;
 
-	private string[] ValidKeys = new string[] {"Up", "Left", "Right", "Down", "Punch", "Kick"};
-	private string[] ValidMoveKeys = new string[] {"Up", "Left", "Right", "Down"};
-	private string[] ValidAttackKeys = new string[] {"Punch", "Kick"};
+	private bool upActive;
+	private bool downActive;
+	private bool leftActive;
+	private bool rightActive;
+	private float axisBuffer = 0.75f;
 	public static List<string> recordedKeys = new List<string>();
+	int index;
 
 	void Awake () {
 		for (int i = 0; i < 30; i++) {
@@ -21,39 +24,60 @@ public class InputReciever : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.anyKey) {
-			foreach (string key in ValidMoveKeys)
-			{
-				if (Input.GetButton(key)) {
-					recordedKeys.Add(key);
-					InputEventArgs args = new InputEventArgs();
-					args.pressedKey = key;
-					args.KeyIndex = recordedKeys.Count - 1;
-					InputDetected(this, args);
-				}
-			}
-			foreach (string key in ValidAttackKeys) {
-				if (Input.GetButton(key)) {
-					recordedKeys.Add(key);
-					InputEventArgs args = new InputEventArgs();
-					args.pressedKey = key;
-					args.KeyIndex = recordedKeys.Count - 1;
-					InputDetected(this, args);
-				}
-			}
-		} else {
+		index = recordedKeys.Count;
+		if (Input.GetAxisRaw("Horizontal") < -axisBuffer) {
+			SendKeyEvent("Left");
+			leftActive = true;
+		}
+		if (Input.GetAxisRaw("Horizontal") > axisBuffer) {
+			SendKeyEvent("Right");
+			rightActive = true;
+		}
+		if (Input.GetAxisRaw("Vertical") < -axisBuffer) {
+			SendKeyEvent("Up");
+			downActive = true;
+		}
+		if (Input.GetAxisRaw("Vertical") > axisBuffer) {
+			SendKeyEvent("Down");
+			upActive = true;
+		}
+		if (Input.GetButton("Punch")) {
+			SendKeyEvent("Punch");
+		}
+		if (Input.GetButtonUp("Punch")) {
+			SendKeyDownEvent("Punch");
+		}
+		else if (Input.GetButton("Kick")) {
+			SendKeyEvent("Kick");
+		}
+		if (Input.GetButtonUp("Kick")) {
+			SendKeyDownEvent("Kick");
+		}
+		if (index == recordedKeys.Count) {
 			recordedKeys.Add("None");
 		}
+	}
 
-		for (int i = 0; i < ValidKeys.Length; i++)
-		{
-			if (Input.GetButtonUp(ValidKeys[i])) {
-				InputEventArgs args = new InputEventArgs();
-				args.pressedKey = ValidKeys[i];
-				InputReleased(this, args);
-			}
-		}
+	private void SendKeyEvent (string key) {
+		recordedKeys.Add(key);
+		InputEventArgs args = new InputEventArgs();
+		args.pressedKey = key;
+		args.KeyIndex = recordedKeys.Count - 1;
+		InputDetected(this, args);
+	}
 
+	private void SendKeyDownEvent (string key) {
+		InputEventArgs args = new InputEventArgs();
+		args.pressedKey = key;
+		InputReleased(this, args);
+	}
+
+	private void RecordKey (string key) {
+		recordedKeys.Add(key);
+		InputEventArgs args = new InputEventArgs();
+		args.pressedKey = key;
+		args.KeyIndex = recordedKeys.Count - 1;
+		InputDetected(this, args);
 	}
 
 	public List<string> ReturnRecordedKeys () {
